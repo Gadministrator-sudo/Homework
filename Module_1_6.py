5bffa6ee549110903a0d4b4d6c6b230019e8d3a8
Анатолий <Anatolich94@gmail.com>
2024-11-30 20:34:14 +0300

    Homework 1.6 Final

--- Module_1_6.py
+++ Module_1_6.py
@@ -3,20 +3,25 @@
 my_dict = {'Tolik' : 1994 , 'Sasha' : 1991}
 print(my_dict)
 
-#  Выведите на экран одно значение по существующему ключу, одно по отсутствующему из словаря my_dict без ошибки.
-# // Не совсем понял вторую часть задания
-#my_dict['Anton'] = 1992
-print(my_dict['Tolik'],['Anton'])
-
+print('Existing value:' , my_dict.get('Tolik'))
+print(my_dict.get('Anton', 'Not existing value'))
 
 my_dict.update({'Vova' : 2021,
                 'Leva' : 2023})
 print(my_dict)
 
 a = my_dict.pop('Tolik')
-print(a)
-print(my_dict)
+print('Deleted value:',a)
+print('Modified dictionary',my_dict)
+
+
 
 # Пункт 3. Работа со множествами
 
-my
+my_set = {1, 2, 3, 2, 1,'Tolik',42.1}
+print('Set', my_set)
+
+my_set.add(5)
+my_set.add('String')
+my_set.discard('Tolik')
+print('Modified set', my_set)
