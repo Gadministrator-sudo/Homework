ac25e60510991153773613e6e8380bc5e2976985
Анатолий <Anatolich94@gmail.com>
2024-12-14 02:35:57 +0300

    Homework

--- Module_2/Module_2_Homework_6_v2.py
+++ Module_2/Module_2_Homework_6_v2.py
@@ -1,13 +1,18 @@
 n = int(input("Введите число от 3 до 20: "))
 list_ = []
-for i in range(1, 21):
-    for j in range(2, 21):
-        if n % (j + i) == 0 and i < j:
-            list_.append(i)
-            list_.append(j)
-            j = j + 1
-        else:
-            continue
-    i = i + 1
-    continue
-print(list_)
+def generator():
+    for i in range(1, 21):
+        for j in range(2, 21):
+            if n % (j + i) == 0 and i < j:
+                list_.append(i)
+                list_.append(j)
+                j = j + 1
+            else:
+                continue
+        i = i + 1
+        continue
+    return
+
+if n > 2 and n < 21:
+    generator()
+    print(list_)
